23f9852adaaf88efb45b0e91edd512cfd239f2cc
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-06-22 20:10:42 -0400

    add ApolloClient, useQuery,  to create-react-a0pp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
 
-function App() {
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+type Rate = {
+  currency: number,
+  rate: number
+}
+
+const ExchangeRates: React.FunctionComponent = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
+  return data.rates.map(({ currency, rate }: Rate) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
     </div>
   );
 }
